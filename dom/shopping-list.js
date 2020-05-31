72c0d3f60aac8766956b37178fdbbf853c5b93c3
Kevin Ibanez <Kpeibanez@gmail.com>
2020-05-31 15:03:21 -0500

    completed exercise 1 of dom exercise, added the list item span and button and added the functionality to delete and add items with focus on input

--- dom/shopping-list.js
+++ dom/shopping-list.js
@@ -1,9 +1,22 @@
 var myList = document.getElementById("my-list");
 var inputItem = document.getElementById("item");
-var items = function userItems () {
-    var input = inputItem.value;
-    alert(input);
-}
 var myButton = document.getElementById("button");
-myButton.addEventListener('click', items);
+myButton.addEventListener('click', function (){
+    var input = inputItem.value;
+    var listItem = document.createElement("LI");
+    var itemSpan = document.createElement("span");
+    var myButton2 = document.createElement("button");
+    listItem.appendChild(itemSpan);
+    listItem.appendChild(myButton2);
+    var text = document.createTextNode(inputItem.value);
+    itemSpan.appendChild(text);
+    var buttonText = document.createTextNode("Delete");
+    inputItem.value = '';
+    myButton2.appendChild(buttonText);
+    myButton2.addEventListener('click', function (){
+        document.getElementById("my-list").removeChild(listItem);
+    });
+    document.getElementById("my-list").appendChild(listItem);
+    document.getElementById("item").focus();
+});
 
