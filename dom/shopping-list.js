34bdc71e335b176843e4daa054f9a32120b98955
Kevin Ibanez <Kpeibanez@gmail.com>
2020-05-31 12:21:11 -0500

    added button onclick function to html and js

---
+++ dom/shopping-list.js
@@ -0,0 +1,7 @@
+var myList = document.getElementById("#my-list");
+var items = document.getElementById("#item");
+
+var myButton = document.getElementById("#button");
+myButton.onclick = myFunction(){
+    document.querySelector("#item").textContent = items;
+}
